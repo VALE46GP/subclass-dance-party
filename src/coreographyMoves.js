02603652bf70f4bf4f7359c9af38a587e7848a5d
Eric Callari <ericcallari1@gmail.com>
2018-09-26 08:06:38 -0700

    added audio

--- src/coreographyMoves.js
+++ src/coreographyMoves.js
@@ -19,6 +19,17 @@ var lineUp = function() {
 };
 
 var adventure = function() {
+
+
+  //audio
+  var audio = document.getElementById('audio');
+
+  var source = document.getElementById('audioSource');
+  source.src = elm.getAttribute('data-value');
+
+  audio.load(); //call this to just preload the audio without playing
+  audio.play(); //call this to play the song right away
+
   
   var rickArray = [];
   var othersArray = [];
